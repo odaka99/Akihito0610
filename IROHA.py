60e043dd4a29f309d80935b28d6530e5cd1e467c
odaka99 <146923968+odaka99@users.noreply.github.com>
2023-10-16 18:39:01 +0900

    Update IROHA.py

--- IROHA.py
+++ IROHA.py
@@ -81,7 +81,7 @@ class Network :
     def T(self, x):
         return self.fftt*x*(1+self.b/(1+self.power)*(x/self.capacity)**self.power)
     def Z(self, x):
-        return sum(self.T(x))
+        return sum(self.T(x))+100
     
     #
     # Shoretst travel time for every O-D pairs (for convergence test)
